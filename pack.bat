be36147a27fd102192ad09f48db1677f0618fb72
jvav-runtime-environment <dannybxx@qq.com>
2024-12-31 19:31:06 +0800

    modified:   pack-onefile.bat 	modified:   pack.bat

--- pack.bat
+++ pack.bat
@@ -1,5 +1,5 @@
 call .venv\Scripts\activate
 
-python -m nuitka --file-version=1.0.3 --standalone --no-pyi-file --jobs=8 --clean-cache=all --lto=yes --plugin-enable=tk-inter,upx --upx-binary=upx.exe --windows-icon-from-ico=link.ico --windows-console-mode=disable --output-filename=connect.exe --include-data-file=icon.png=icon.png main.py
+python -m nuitka --file-version=1.1.0 --standalone --no-pyi-file --jobs=8 --clean-cache=all --lto=yes --plugin-enable=tk-inter,upx --upx-binary=upx.exe --windows-icon-from-ico=link.ico --windows-console-mode=disable --output-filename=connect.exe --include-data-file=icon.png=icon.png main.py
 
 pause
